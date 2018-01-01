825217de5b611771729a3852836af9cf3299f6d4
autopp <autopp.inc@gmail.com>
2018-01-01 20:26:50 +0900

    add statement node definitions

--- src/main/scala/Ast.scala
+++ src/main/scala/Ast.scala
@@ -2,5 +2,37 @@ package indentp
 
 sealed abstract class Ast
 
+/*
+  Program: (Stmt)*
+ */
+case class Program(stmts: List[Stmt])
+
+/*
+  Stmt: ExprStmt
+      | IfStmt
+      | WhileStmt
+      | PassStmt
+ */
 abstract class Stmt extends Ast
+
+/*
+  ExprStmt: Expr
+ */
+case class ExprStmt(body: Expr) extends Stmt
+
+/*
+  IfStmt: `if` `(` Expr `)` (Stmt)+
+ */
+case class IfStmt(cond: Expr, stmts: List[Stmt]) extends Stmt
+
+/*
+  WhileStmt: `while` `(` Expr `)` (Stmt)+
+ */
+case class WhileStmt(cond: Expr, stmts: List[Stmt]) extends Stmt
+
+/*
+  PassStmt: `pass`
+ */
+case object PassStmt extends Stmt
+
 abstract class Expr extends Ast
