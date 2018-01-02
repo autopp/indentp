9a75df47ba89ce3d09e5ea6aeb080c9026c9904a
autopp <autopp.inc@gmail.com>
2018-01-02 23:11:22 +0900

    add alias to Either[String, A]

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -22,14 +22,16 @@ case object IndentToken extends Token("INDENT")
 case object DedentToken extends Token("DEDENT")
 
 class Parser {
-  def parse(source: String): Either[String, Ast] = {
+  type MayError[A] = Either[String, A]
+
+  def parse(source: String): MayError[Ast] = {
     new Lexer().tokenize(source) match {
       case Right(tokens) => parseProgram(tokens, Nil)
       case Left(msg) => Left(msg)
     }
   }
 
-  def parseProgram(tokens: List[Token], buf: List[Stmt]): Either[String, Program] = {
+  def parseProgram(tokens: List[Token], buf: List[Stmt]): MayError[Program] = {
     tokens match {
       case Nil => {
         Right(Program(buf.reverse))
@@ -43,110 +45,110 @@ class Parser {
     }
   }
 
-  def parseStmt(tokens: List[Token]): Either[String, (Stmt, List[Token])] = {
+  def parseStmt(tokens: List[Token]): MayError[(Stmt, List[Token])] = {
     Left("parseStmt is not implemented")
   }
-}
 
-class Lexer {
-  val rules = List[(Regex, String => Option[Token])](
-    ("[ ]".r, (s) => None),
-    ("[0-9]+".r, (s) => Some(NumToken(s))),
-    ("[a-zA-Z_][a-zA-Z_0-9]*".r, (s) => Some(NameToken(s))),
-    ("[(]".r, (s) => Some(LeftParenToken)),
-    ("[)]".r, (s) => Some(RightParenToken)),
-    (",".r, (s) => Some(CommaToken)),
-    (":".r, (s) => Some(ColonToken)),
-    ("==".r, (s) => Some(EqualOpToken)),
-    ("=".r, (s) => Some(AssignOpToken)),
-    ("[+]".r, (s) => Some(AddOpToken)),
-    ("[*]".r, (s) => Some(MulOpToken)),
-    ("[-]".r, (s) => Some(NegOpToken)),
-    ("[!]".r, (s) => Some(NotOpToken)),
-    ("if".r, (s) => Some(IfToken)),
-    ("while".r, (s) => Some(WhileToken))
-  )
+  class Lexer {
+    val rules = List[(Regex, String => Option[Token])](
+      ("[ ]".r, (s) => None),
+      ("[0-9]+".r, (s) => Some(NumToken(s))),
+      ("[a-zA-Z_][a-zA-Z_0-9]*".r, (s) => Some(NameToken(s))),
+      ("[(]".r, (s) => Some(LeftParenToken)),
+      ("[)]".r, (s) => Some(RightParenToken)),
+      (",".r, (s) => Some(CommaToken)),
+      (":".r, (s) => Some(ColonToken)),
+      ("==".r, (s) => Some(EqualOpToken)),
+      ("=".r, (s) => Some(AssignOpToken)),
+      ("[+]".r, (s) => Some(AddOpToken)),
+      ("[*]".r, (s) => Some(MulOpToken)),
+      ("[-]".r, (s) => Some(NegOpToken)),
+      ("[!]".r, (s) => Some(NotOpToken)),
+      ("if".r, (s) => Some(IfToken)),
+      ("while".r, (s) => Some(WhileToken))
+    )
 
-  def tokenize(source: String): Either[String, List[Token]] = {
-    // Ignore empty lines
-    val emptyPattern = "\\s*$".r
-    val lines = source.lines.filter(emptyPattern.findPrefixOf(_) == None).toList
-    val indentStack = List(0)
-    tokenizeLines(lines, indentStack, List())
-  }
+    def tokenize(source: String): MayError[List[Token]] = {
+      // Ignore empty lines
+      val emptyPattern = "\\s*$".r
+      val lines = source.lines.filter(emptyPattern.findPrefixOf(_) == None).toList
+      val indentStack = List(0)
+      tokenizeLines(lines, indentStack, List())
+    }
 
-  def tokenizeLines(lines: List[String], indentStack: List[Int], buf: List[Token]): Either[String, List[Token]] = {
-    lines match {
-      case Nil => {
-        Right((List.fill(indentStack.size - 1)(DedentToken) ++ buf).reverse)
+    def tokenizeLines(lines: List[String], indentStack: List[Int], buf: List[Token]): MayError[List[Token]] = {
+      lines match {
+        case Nil => {
+          Right((List.fill(indentStack.size - 1)(DedentToken) ++ buf).reverse)
+        }
+        case line::rest => {
+          tokenizeLine(line, indentStack, buf) match {
+            case Right((indentStack, buf)) => tokenizeLines(rest, indentStack, NewlineToken::buf)
+            case Left(msg) => Left(msg)
+          }
+        }
       }
-      case line::rest => {
-        tokenizeLine(line, indentStack, buf) match {
-          case Right((indentStack, buf)) => tokenizeLines(rest, indentStack, NewlineToken::buf)
+    }
+
+    def tokenizeLine(line: String, indentStack: List[Int], buf: List[Token]): MayError[(List[Int], List[Token])] = {
+      val pattern = "^(\\s*)(\\S.*)$".r
+      line match {
+        case pattern(indent, body) => {
+          tokenizeIndent(indent.size, indentStack, buf) match {
+            case Right((indentStack, buf)) => {
+              tokenizeLineBody(body, buf) match {
+                case Right(buf) => Right((indentStack, buf))
+                case Left(e) => Left(e)
+              }
+            }
+            case e => e
+          }
+        }
+      }
+    }
+
+    def tokenizeIndent(indentSize: Int, indentStack: List[Int], buf: List[Token], topLevel: Boolean = true): MayError[(List[Int], List[Token])] = {
+      indentStack match {
+        case lastIndent::restStack => {
+          indentSize match {
+            case n if n > lastIndent => {
+              if (topLevel) Right(n::indentStack, IndentToken::buf) else Left("inconsistent dedent")
+            }
+            case n if n == lastIndent => Right(indentStack, buf)
+            case _ => {
+              tokenizeIndent(indentSize, restStack, DedentToken::buf, false)
+            }
+          }
+        }
+        case _ => {
+          Left("BUG: indentStack is empty")
+        }
+      }
+    }
+
+    def tokenizeLineBody(body: String, buf: List[Token]): MayError[(List[Token])] = {
+      if (body.isEmpty) {
+        Right(buf)
+      } else {
+        applyRule(body, rules, buf) match {
+          case Right((rest, buf)) => tokenizeLineBody(rest, buf)
           case Left(msg) => Left(msg)
         }
       }
     }
-  }
 
-  def tokenizeLine(line: String, indentStack: List[Int], buf: List[Token]): Either[String, (List[Int], List[Token])] = {
-    val pattern = "^(\\s*)(\\S.*)$".r
-    line match {
-      case pattern(indent, body) => {
-        tokenizeIndent(indent.size, indentStack, buf) match {
-          case Right((indentStack, buf)) => {
-            tokenizeLineBody(body, buf) match {
-              case Right(buf) => Right((indentStack, buf))
-              case Left(e) => Left(e)
-            }
-          }
-          case e => e
-        }
-      }
-    }
-  }
-
-  def tokenizeIndent(indentSize: Int, indentStack: List[Int], buf: List[Token], topLevel: Boolean = true): Either[String, (List[Int], List[Token])] = {
-    indentStack match {
-      case lastIndent::restStack => {
-        indentSize match {
-          case n if n > lastIndent => {
-            if (topLevel) Right(n::indentStack, IndentToken::buf) else Left("inconsistent dedent")
-          }
-          case n if n == lastIndent => Right(indentStack, buf)
-          case _ => {
-            tokenizeIndent(indentSize, restStack, DedentToken::buf, false)
-          }
-        }
-      }
-      case _ => {
-        Left("BUG: indentStack is empty")
-      }
-    }
-  }
-
-  def tokenizeLineBody(body: String, buf: List[Token]): Either[String, (List[Token])] = {
-    if (body.isEmpty) {
-      Right(buf)
-    } else {
-      applyRule(body, rules, buf) match {
-        case Right((rest, buf)) => tokenizeLineBody(rest, buf)
-        case Left(msg) => Left(msg)
-      }
-    }
-  }
-
-  def applyRule(body: String, rules: List[(Regex, String => Option[Token])], buf: List[Token]): Either[String, (String, List[Token])] = {
-    rules match {
-      case Nil => Left(s"cannot recognize `${body.head}`")
-      case (pattern, converter)::restRules => {
-        pattern.findPrefixOf(body) match {
-          case None => applyRule(body, restRules, buf)
-          case Some(matched) => {
-            val restBody = body.drop(matched.length)
-            converter(matched) match {
-              case None => Right((restBody, buf))
-              case Some(token) => Right((restBody, token::buf))
+    def applyRule(body: String, rules: List[(Regex, String => Option[Token])], buf: List[Token]): MayError[(String, List[Token])] = {
+      rules match {
+        case Nil => Left(s"cannot recognize `${body.head}`")
+        case (pattern, converter)::restRules => {
+          pattern.findPrefixOf(body) match {
+            case None => applyRule(body, restRules, buf)
+            case Some(matched) => {
+              val restBody = body.drop(matched.length)
+              converter(matched) match {
+                case None => Right((restBody, buf))
+                case Some(token) => Right((restBody, token::buf))
+              }
             }
           }
         }
