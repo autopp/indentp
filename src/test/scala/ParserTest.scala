bf8e365c98c5c2fc24d073ac6e946c4ce9c48e70
autopp <autopp.inc@gmail.com>
2018-01-02 14:04:54 +0900

    fix test

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -8,7 +8,6 @@ class ParserTest extends FunSpec {
     describe("parse") {
       describe("with `x = 42`") {
         it("returns AssignStmt") {
-          pending
           val stmt = ExprStmt(AssignExpr("x", Num(42)))
           parser.parse("x = 42") should equal(Right(Program(List(stmt))))
         }
@@ -16,7 +15,6 @@ class ParserTest extends FunSpec {
 
       describe("with arith expr") {
         it("returns Expr") {
-          pending
           val stmt = ExprStmt(BinOpExpr("+", Num(1), BinOpExpr("*", Num(2), UnOpExpr("-", Num(3)))))
           parser.parse("1 + 2 * -3") should equal(Right(Program(List(stmt))))
         }
@@ -24,7 +22,6 @@ class ParserTest extends FunSpec {
 
       describe("with function call expr") {
         it("returns Expr") {
-          pending
           val stmt = ExprStmt(CallExpr(CallExpr(CallExpr(Var("f"), List()), List(Var("x"))), List(Var("y"), Var("z"))))
           parser.parse("f()(x)(y, z)") should equal(Right(Program(List(stmt))))
         }
@@ -32,7 +29,6 @@ class ParserTest extends FunSpec {
 
       describe("with logical expr") {
         it("returns Expr") {
-          pending
           val stmt = ExprStmt(UnOpExpr("!", BinOpExpr("==", Var("x"), BinOpExpr("+", Var("y"), Num(42)))))
           parser.parse("!(x == y + 42)") should equal(Right(Program(List(stmt))))
         }
@@ -40,16 +36,14 @@ class ParserTest extends FunSpec {
 
       describe("with multiline code `x = 42\\nf()`") {
         it("returns two Expr") {
-          pending
           val first = ExprStmt(AssignExpr("x", Num(42)))
           val second = ExprStmt(CallExpr(Var("f"), List()))
-          parser.parse("x = 42\\nf()`") should equal(Right(Program(List(first, second))))
+          parser.parse("x = 42\nf()") should equal(Right(Program(List(first, second))))
         }
       }
 
       describe("with pass stmt") {
         it("returns PassStmt") {
-          pending
           parser.parse("pass") should equal(Right(Program(List(PassStmt))))
         }
       }
